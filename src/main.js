6662642907013cc5ab37b6e9662b7ce7fd2163c0
wannapornM <wannaporndangkum@gmail.com>
2022-05-07 15:30:34 +0700

    Install react and react-dom into project and try to render basic component

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log("Why not auto reload?");
+import ReactDOM from "react-dom";
+import React from "react";
+//import { App } from "./App";
+
+function Main() {
+  return <div>Hello from Main component</div>;
+}
+
+const main = document.getElementById("main");
+ReactDOM.render(<Main />, main);
